733146a86245b3dc7e24460aadd099010fba3d78
VincenzoLivigni <enzolivigni13@gmail.com>
2025-10-23 14:38:43 +0200

    with a loop multiply random email addresses

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api
 generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 */
 
+// con un loop moltiplico gli indirizzi email randomici
+for (let i = 0; i < 10; i++) {
 
+// genero un indirizzo mail randomico 
 axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
 .then((res) => {
 console.log(res.data.response);
 
 })
-.catch((err) => {
-console.log(err);
-
-})
-
+}
