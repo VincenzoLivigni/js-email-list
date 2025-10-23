d13882b42b9cd2884d536e140dc3e684fbc56f5b
VincenzoLivigni <enzolivigni13@gmail.com>
2025-10-23 15:24:38 +0200

    print the 10 emails in a list on the screen

--- script.js
+++ script.js
@@ -4,13 +4,20 @@ Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api
 generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 */
 
+// creo elemento della DOM
+const listaEl = document.getElementById("lista")
+
 // con un loop moltiplico gli indirizzi email randomici
 for (let i = 0; i < 10; i++) {
 
 // genero un indirizzo mail randomico 
 axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
 .then((res) => {
-console.log(res.data.response);
+    // creo una variabile d'appoggio nella quale aggiungo la mail
+       const randomEmail = res.data.response;
+       console.log(randomEmail);
 
+// stampo a schermo le 10 email in una lista
+listaEl.innerHTML += `<li>${randomEmail}</li>`;
 })
 }
