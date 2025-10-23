65de9aeb719f3a28039d3fb976c2eacf3edc78f2
VincenzoLivigni <enzolivigni13@gmail.com>
2025-10-23 14:28:53 +0200

    generate a random email address through API

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("AJAX");
+/*
+Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+*/
+
+
+axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+.then((res) => {
+console.log(res.data.response);
+
+})
+.catch((err) => {
+console.log(err);
+
+})
+
